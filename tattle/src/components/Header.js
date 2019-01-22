339b9a173c18d269a164c262b34625c358e06147
Zoe <zoe.gelech@gmail.com>
2019-01-22 14:11:10 -0800

    made componets for each layout section

---
+++ tattle/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import '../styles_css/Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="mainHeader">
+        <h4>Twitter Clone</h4>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ tattle/src/components/Tweets.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import '../styles_css/Tweets.css';
+
+class Tweets extends Component {
+  render() {
+    return (
+      <div className="mainTweets">
+
+        Extraction mocha variety extra cup barista café au lait sweet. Brewed iced froth pumpkin spice galão, extra id cultivar frappuccino cappuccino. To go rich grinder, rich robusta mazagran pumpkin spice blue mountain. Brewed bar latte body crema and carajillo. Breve, organic ristretto organic aromatic foam eu flavour decaffeinated and est doppio.
+
+        Latte, white eu robusta ut breve whipped white froth robusta spoon. Aged aromatic lungo id, shop milk plunger pot cinnamon that. Fair trade espresso, wings, filter, extraction trifecta cup cinnamon extra whipped percolator. Redeye breve aroma crema flavour french press barista in plunger pot. Coffee iced at shop steamed irish java.
+
+        Milk organic carajillo espresso acerbic robust dripper id grounds. Cinnamon mocha, cream, medium, flavour, aftertaste, pumpkin spice roast spoon sugar latte. Robust, aftertaste, froth turkish to go robust filter. Turkish pumpkin spice, seasonal sugar affogato macchiato saucer that shop wings. Single origin blue mountain seasonal, qui redeye, trifecta half and half so caffeine grounds.
+
+        Cup, to go whipped, java spoon beans variety coffee. Percolator irish, french press extraction, flavour, blue mountain sugar breve et as ristretto. Java espresso acerbic aroma sit instant milk single origin foam redeye aftertaste. Dark, doppio sweet ristretto rich eu seasonal. Filter, decaffeinated bar dripper, extra java percolator seasonal arabica cappuccino aromatic coffee.
+
+      </div>
+    );
+  }
+}
+
+export default Tweets;
