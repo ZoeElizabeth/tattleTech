3b92866e3dc8a6eb64474c1ca89c0c2c11b9c809
Zoe <zoe.gelech@gmail.com>
2019-01-22 13:40:50 -0800

    installed sass and set up base flexbox template to start from

--- tattle/src/App.js
+++ tattle/src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './styles_css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <body>
+        <header>Header</header>
+        <div className="main">
+          <article>
+            Extraction mocha variety extra cup barista café au lait sweet. Brewed iced froth pumpkin spice galão, extra id cultivar frappuccino cappuccino. To go rich grinder, rich robusta mazagran pumpkin spice blue mountain. Brewed bar latte body crema and carajillo. Breve, organic ristretto organic aromatic foam eu flavour decaffeinated and est doppio.
+
+            Latte, white eu robusta ut breve whipped white froth robusta spoon. Aged aromatic lungo id, shop milk plunger pot cinnamon that. Fair trade espresso, wings, filter, extraction trifecta cup cinnamon extra whipped percolator. Redeye breve aroma crema flavour french press barista in plunger pot. Coffee iced at shop steamed irish java.
+
+            Milk organic carajillo espresso acerbic robust dripper id grounds. Cinnamon mocha, cream, medium, flavour, aftertaste, pumpkin spice roast spoon sugar latte. Robust, aftertaste, froth turkish to go robust filter. Turkish pumpkin spice, seasonal sugar affogato macchiato saucer that shop wings. Single origin blue mountain seasonal, qui redeye, trifecta half and half so caffeine grounds.
+
+            Cup, to go whipped, java spoon beans variety coffee. Percolator irish, french press extraction, flavour, blue mountain sugar breve et as ristretto. Java espresso acerbic aroma sit instant milk single origin foam redeye aftertaste. Dark, doppio sweet ristretto rich eu seasonal. Filter, decaffeinated bar dripper, extra java percolator seasonal arabica cappuccino aromatic coffee.
+          </article>
+          <aside>
+            Cupcake ipsum dolor sit amet cupcake gingerbread dessert. Tiramisu cake cake marzipan donut chocolate chocolate gingerbread cotton candy. Gummi bears tiramisu jujubes jelly cookie lollipop brownie sugar plum danish.
+
+            Topping ice cream candy canes tootsie roll jelly-o tiramisu gummi bears tootsie roll. Tart cake pudding fruitcake dragée sweet. Gummies pudding carrot cake pudding macaroon ice cream. Apple pie powder liquorice apple pie croissant sweet roll sweet roll carrot cake cake.
+
+            Pie gingerbread chocolate bar fruitcake chocolate bar apple pie. Marzipan pudding brownie liquorice sesame snaps brownie toffee marzipan chupa chups. Liquorice donut bear claw bear claw gummies. Pie gingerbread cupcake jelly cake chocolate cupcake gingerbread tootsie roll.
+
+            Donut fruitcake jelly beans brownie chocolate caramels lemon drops chocolate sweet. Donut ice cream chocolate bar chupa chups macaroon gummi bears jujubes jelly beans chupa chups. Macaroon jelly beans biscuit muffin muffin. Tiramisu bonbon brownie macaroon powder sweet.
+
+            Danish cake chocolate sweet ice cream donut jujubes cake sesame snaps. Candy canes cotton candy muffin chocolate biscuit. Chupa chups cookie chocolate pie icing
+          </aside>
+        </div>
+      </body>
     );
   }
 }
